87bbea34b473adee8e7fce9198da95b3f3429792
Ryan Irish <ryancirish@users.noreply.github.com>
2024-08-30 19:49:24 -0400

    fix page controller

--- lib/alpina_web/controllers/page_controller.ex
+++ lib/alpina_web/controllers/page_controller.ex
@@ -10,12 +10,12 @@ defmodule AlpinaWeb.PageController do
   def blog(conn, _params) do
     conn
     |> put_resp_content_type("text/html")
-    |> send_resp(200, File.read!(Application.app_dir(:alpina, "priv/static/blog.html")))
+    |> send_resp(200, File.read!(Application.app_dir(:alpina, "priv/static/assets/pages/blog.html")))
   end
 
   def shop(conn, _params) do
     conn
     |> put_resp_content_type("text/html")
-    |> send_resp(200, File.read!(Application.app_dir(:alpina, "priv/static/shop.html")))
+    |> send_resp(200, File.read!(Application.app_dir(:alpina, "priv/static/assets/pages/shop.html")))
   end  
 end
